8d342b7a2c752a9120bc3cd40c32657f39411abc
Aleksey Grushenkov <153802645+Grugrusha96@users.noreply.github.com>
2023-12-21 01:11:11 +0300

    lesson1 commit

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,30 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-}
+//    import java.util.Scanner;
+//
+//    public class Main {
+//        public static void main(String[] args) {
+//            Scanner scanner = new Scanner(System.in);
+//
+//            String name = scanner.nextLine();
+//            String str = "Hello " + name;
+//            System.out.println(str);
+//        }
+//public class main {
+//    public main() {
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println("hellow World!");
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println();
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        System.out.println("сложно пока что:)" + (a / b));
+//        System.out.println();
+//    }
+//}
